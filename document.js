17a3bbe5addc954e67dac6b0490b4d1566c02d97
davirrocha <davirochamoraes7@gmail.com>
2024-04-12 23:54:04 -0300

    first-commit

---
+++ document.js
@@ -0,0 +1,34 @@
+
+
+
+let click = document.querySelector(".sortear")
+
+
+function clickButton() {
+
+    const min = Math.ceil(document.querySelector(".left").value)
+    const max = Math.floor(document.querySelector(".right").value)
+    const number = document.querySelector(".result")
+    const result = Math.floor(Math.random() * (max - min) + min);
+
+    number.innerHTML = result
+
+    if (max >= min) {
+        console.log("valor correto")
+        number.style.color = "green"
+    } else {
+        number.innerHTML = "ERRO!! O valor MÍNIMO tem que ser MENOR que o valor MÁXIMO "
+        number.style.color = "red"
+    }
+
+}
+
+click.addEventListener("click", clickButton)
+
+
+
+
+
+
+
+
